f5d9c724df72d466ba9e1dba0e4eda7012f77166
D3stinn3 <paynedestinne@gmail.com>
2023-04-18 11:33:14 +0300

    System

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-PASSWORD = "Trinity"
-EMAIL_ADDR = "paynedestinne@gmail.com"
-API_KEY = "destiny"
+from dotenv import load_dotenv
+import os
+
+"""Loading Environment Variables"""
+
+load_dotenv()
+
+PASSWORD = os.getenv("PASSWORD")
+EMAIL_ADDR = os.getenv("EMAIL_ADDR")
+API_KEY = os.getenv("API_KEY")
 
 url = f"https://www.d3stinn3.net/api/?api_key={API_KEY}"
 
 login_info = {
     "email": EMAIL_ADDR,
-    "password": PASSWORD
+    "password": PASSWORD,
+    "api_key": API_KEY
 }
+
+print(login_info)
