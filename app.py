71b81ebfe751870110c7c0b15880e2ccdf62ae82
D3stinn3 <paynedestinne@gmail.com>
2023-04-18 11:12:19 +0300

    Application

---
+++ app.py
@@ -0,0 +1,10 @@
+PASSWORD = "Trinity"
+EMAIL_ADDR = "paynedestinne@gmail.com"
+API_KEY = "destiny"
+
+url = f"https://www.d3stinn3.net/api/?api_key={API_KEY}"
+
+login_info = {
+    "email": EMAIL_ADDR,
+    "password": PASSWORD
+}
